9c29550d52ce0f1dc5c6bd22a15e6eb4926393ff
FerrrazSenpai <50244015+FerrrazSenpai@users.noreply.github.com>
2019-05-10 15:43:43 +0100

    Update Aeronave.php
    testCommit

--- app/Aeronave.php
+++ app/Aeronave.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Aeronave extends Model
 {
+    use SoftDeletes;
+
 	protected $primaryKey = 'matricula';	//Por default ele vai à procura do 'id'
 
     public $incrementing = false;	//Isto impedirá que ele seja um campo de incremento automático
